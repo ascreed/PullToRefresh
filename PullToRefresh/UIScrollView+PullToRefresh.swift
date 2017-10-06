b1c52d7b6a69ee1eb08298fa1ebb356b95dfcd97
Y.KEISUKE <yamagutikeisuke@gmail.com>
2017-10-06 18:56:04 +0900

    Change color for debug

--- PullToRefresh/UIScrollView+PullToRefresh.swift
+++ PullToRefresh/UIScrollView+PullToRefresh.swift
@@ -55,7 +55,7 @@ public extension UIScrollView {
         }
         
         view.frame = CGRect(x: 0, y: originY, width: frame.width, height: view.frame.height)
-        
+        view.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         addSubview(view)
         sendSubview(toBack: view)
     }
